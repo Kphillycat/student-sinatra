f9fe49c8cdac0b0209cfe08d034a6070e7c93f7e
Kphillycat <keithwilliamsjr@gmail.com>
2013-12-09 12:44:08 -0500

    Add views for hello.erb and artists.erb

--- app.rb
+++ app.rb
@@ -6,5 +6,18 @@ module StudentSite
     get '/' do
       "hello world!"
     end
+
+    get '/hello-world' do
+    	@random_numbers = []
+    	20.times do |i|
+    		@random_numbers << i+1
+    	end
+    	erb :hello
+    end
+
+    get '/artists' do
+		@artists = ["Macklemore", "Childish Gambino", "Frank Sinatra", "Rebecca Black"]
+		erb :artists
+	end
   end
 end
